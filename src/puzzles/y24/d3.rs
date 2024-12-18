56444b23fdf9e2cc7bd658e804b50dc68ad1f3ee
Eiji Ensing <eijitron@gmail.com>
2024-12-18 12:39:58 +0100

    24 day 3 part 2

--- src/puzzles/y24/d3.rs
+++ src/puzzles/y24/d3.rs
@@ -35,6 +35,56 @@ fn multiply_line(line: &str) -> u32 {
     result
 }
 
+fn conditional_multiply_line(line: &str, execute_next: &mut bool) -> (u32, bool) {
+    let mut chars = line.chars().peekable();
+    let mut result = 0;
+    while chars.peek().is_some() {
+        let mut current = chars.clone();
+        let possibledo = chars.clone().by_ref().take(4).collect::<String>();
+        let possibledont = chars.clone().by_ref().take(7).collect::<String>();
+
+        if possibledo == "do()" {
+            *execute_next = true;
+        }
+        if possibledont == "don't()" {
+            *execute_next = false;
+        }
+        if current.by_ref().take(4).collect::<String>() != "mul(" {
+            chars.next();
+            continue;
+        }
+        if !*execute_next {
+            chars.next();
+            continue;
+        }
+        chars.by_ref().take(4).for_each(drop);
+
+        let mut current = chars.clone();
+        let first_number: String = current.by_ref().take_while(|&c| c.is_numeric()).collect();
+        if first_number.is_empty() {
+            continue;
+        }
+        chars.by_ref().take(first_number.len()).for_each(drop);
+
+        if chars.next() != Some(',') {
+            continue;
+        }
+
+        let mut current = chars.clone();
+        let second_number: String = current.by_ref().take_while(|&c| c.is_numeric()).collect();
+        if second_number.is_empty() {
+            continue;
+        }
+        chars.by_ref().take(second_number.len()).for_each(drop);
+
+        if chars.next() != Some(')') {
+            continue;
+        }
+        result += first_number.parse::<u32>().unwrap() * second_number.parse::<u32>().unwrap();
+    }
+    (result, *execute_next)
+}
+
 fn part1(input: &str) -> u32 {
     let mut results = 0;
     for line in input.lines() {
@@ -44,7 +94,12 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    0
+    let mut results = 0;
+    let mut execute_next = true;
+    for line in input.lines() {
+        results += conditional_multiply_line(line, &mut execute_next).0;
+    }
+    results
 }
 
 pub fn run() {
@@ -82,8 +137,8 @@ mod tests {
     #[test]
     // Year 2024 Day 3 Part 2
     fn y24d3p2() {
-        let input = "";
-        let expected = 0;
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        let expected = 48;
         assert_eq!(part2(input), expected);
     }
 }
