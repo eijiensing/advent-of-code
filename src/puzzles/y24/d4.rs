be7ae54d046e458dd98ee46e20e8b14314c8bb07
Eiji Ensing <eijitron@gmail.com>
2024-12-18 13:36:49 +0100

    24 day 4 part 2

--- src/puzzles/y24/d4.rs
+++ src/puzzles/y24/d4.rs
@@ -19,47 +19,56 @@ fn check_char(line: &str, index: usize, search: &str, search_index: usize) -> bo
 fn check_x_mas(
     index: usize,
     reversed: bool,
-    first_previous_line: &str,
-    second_previous_line: &str,
-) -> Vec<usize> {
-    let mut correct_indexes_in_second: Vec<usize> = vec![];
+    current_line: &str,
+    next_line: &str,
+    next_next_line: &str,
+) -> u32 {
     let search = if reversed { "SAM" } else { "MAS" };
-    if first_previous_line.len() - index > 3
-        && check_char(first_previous_line, index + 1, search, 1)
-        && check_char(second_previous_line, index + 2, search, 2)
-        && check_char(second_previous_line, index + 2, search, 2)
+    if current_line.len() - index > 2
+        && check_char(next_line, index + 1, search, 1)
+        && check_char(current_line, index + 2, search, 0)
+        && check_char(next_next_line, index + 2, search, 2)
+        && check_char(next_next_line, index, search, 2)
     {
-        correct_indexes_in_second.push(index + 3);
+        return 1;
+    }
+    if current_line.len() - index > 2
+        && check_char(next_line, index + 1, search, 1)
+        && check_char(current_line, index + 2, search, 2)
+        && check_char(next_next_line, index + 2, search, 2)
+        && check_char(next_next_line, index, search, 0)
+    {
+        return 1;
     }
 
-    correct_indexes_in_second
+    0
 }
 fn check_xmas(
     index: usize,
     reversed: bool,
-    first_previous_line: &str,
-    second_previous_line: &str,
-    third_previous_line: &str,
+    next_line: &str,
+    next_next_line: &str,
+    next_next_next_line: &str,
 ) -> Vec<usize> {
     let mut correct_indexes_in_third: Vec<usize> = vec![];
     let search = if reversed { "SAMX" } else { "XMAS" };
-    if check_char(first_previous_line, index, search, 1)
-        && check_char(second_previous_line, index, search, 2)
-        && check_char(third_previous_line, index, search, 3)
+    if check_char(next_line, index, search, 1)
+        && check_char(next_next_line, index, search, 2)
+        && check_char(next_next_next_line, index, search, 3)
     {
         correct_indexes_in_third.push(index);
     }
     if index > 2
-        && check_char(first_previous_line, index - 1, search, 1)
-        && check_char(second_previous_line, index - 2, search, 2)
-        && check_char(third_previous_line, index - 3, search, 3)
+        && check_char(next_line, index - 1, search, 1)
+        && check_char(next_next_line, index - 2, search, 2)
+        && check_char(next_next_next_line, index - 3, search, 3)
     {
         correct_indexes_in_third.push(index - 3);
     }
-    if first_previous_line.len() - index > 3
-        && check_char(first_previous_line, index + 1, search, 1)
-        && check_char(second_previous_line, index + 2, search, 2)
-        && check_char(third_previous_line, index + 3, search, 3)
+    if next_line.len() - index > 3
+        && check_char(next_line, index + 1, search, 1)
+        && check_char(next_next_line, index + 2, search, 2)
+        && check_char(next_next_next_line, index + 3, search, 3)
     {
         correct_indexes_in_third.push(index + 3);
     }
@@ -73,18 +82,19 @@ fn part1(input: &str) -> u32 {
     let mut first_previous_line: Option<&str> = None;
     let mut checked_lines: HashMap<usize, HashSet<usize>> = HashMap::new();
     for (line_index, line) in input.lines().enumerate() {
+        checked_lines.entry(line_index).or_default(); // make sure line index always exists
         appearances += check_horizontal(line);
         if let Some(third) = third_previous_line {
             for (index, character) in third.chars().enumerate() {
-                if (character == 'S' || character == 'M')
+                if (character == 'S' || character == 'X')
                     && checked_lines
                         .get(&line_index)
                         .is_some_and(|set| set.get(&index).is_none())
                 {
-                    let is_second = character == 'S';
+                    let reversed = character == 'S';
                     let result = check_xmas(
                         index,
-                        is_second,
+                        reversed,
                         second_previous_line.unwrap(),
                         first_previous_line.unwrap(),
                         line,
@@ -109,23 +119,19 @@ fn part2(input: &str) -> u32 {
     let mut appearances = 0;
     let mut second_previous_line: Option<&str> = None;
     let mut first_previous_line: Option<&str> = None;
-    let mut checked_lines: HashMap<usize, HashSet<usize>> = HashMap::new();
-    for (line_index, line) in input.lines().enumerate() {
+    for line in input.lines() {
         if let Some(second) = second_previous_line {
             for (index, character) in second.chars().enumerate() {
-                if (character == 'S' || character == 'M')
-                    && checked_lines
-                        .get(&line_index)
-                        .is_some_and(|set| set.get(&index).is_none())
-                {
+                if character == 'S' || character == 'M' {
                     let is_second = character == 'S';
-                    let result = check_x_mas(index, is_second, first_previous_line.unwrap(), line);
-                    checked_lines
-                        .entry(line_index)
-                        .and_modify(|current_checked| {
-                            current_checked.insert(index);
-                        });
-                    appearances += result.len() as u32;
+                    let result = check_x_mas(
+                        index,
+                        is_second,
+                        second_previous_line.unwrap(),
+                        first_previous_line.unwrap(),
+                        line,
+                    );
+                    appearances += result
                 }
             }
         }
@@ -166,6 +172,12 @@ MXMXAXMASX";
         assert_eq!(part1(input), expected);
     }
     #[test]
+    fn y24d4p1right() {
+        let input = "XMAS";
+        let expected = 1;
+        assert_eq!(part1(input), expected);
+    }
+    #[test]
     fn y24d4p1down() {
         let input = "X
 M
@@ -178,8 +190,17 @@ S";
     #[test]
     // Year 2024 Day 4 Part 2
     fn y24d4p2() {
-        let input = "";
-        let expected = 0;
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        let expected = 9;
         assert_eq!(part2(input), expected);
     }
 }
